00804331159ede0c0e72e15fa012d2073cd00ee4
JALewis <lewis.josepha.701@gmail.com>
2015-02-04 06:37:55 -0800

    Internet Check making sure that the user has internet enabled and connected before trying to access the api

--- Android/FunctionFIRST/app/src/main/java/com/vandenrobotics/functionfirst/MainActivity.java
+++ Android/FunctionFIRST/app/src/main/java/com/vandenrobotics/functionfirst/MainActivity.java
@@ -2,8 +2,11 @@ package com.vandenrobotics.functionfirst;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 
@@ -42,24 +45,35 @@ public class MainActivity extends Activity {
      */
     public void activityScout(View view) throws JSONException {
 
-        // no need to pass a year to the API, as it will default to the current year, which is always what we want
-        TheBlueAllianceRestClient.get(this, "events/", TheBlueAllianceRestClient.GET_HEADER, null, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray events) {
-                // handle the incoming JSONArray of events and create a dialog with a list view
-                try {
-                    eventList = JSONArrayParser(events);
-                    sortJSONArray(eventList, "start_date");
-                } catch (JSONException e) {
-                    e.printStackTrace();
+        /*  check the status of the internet connection
+        --> on no connection, load a list of competitions that have already been downloaded
+        --> on no downloaded competitions, prompt the user to enable internet in order to download a comp
+        --> on internet connection, list downloaded competitions first, then all competitions by date
+        --> uses The Blue Alliance API to grab data for all events
+        */
+        if(isOnline()) {
+            TheBlueAllianceRestClient.get(this, "events/", TheBlueAllianceRestClient.GET_HEADER, null, new JsonHttpResponseHandler() {
+                // no need to pass a year to the API, as it will default to the current year, which is always what we want
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray events) {
+                    // handle the incoming JSONArray of events and create a dialog with a list view
+                    try {
+                        eventList = JSONArrayParser(events);
+                        sortJSONArray(eventList, "start_date");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            if (eventList != null) {
+                System.out.println(eventList.size());
+                for (int i = 0; i < eventList.size(); i++) {
+                    System.out.println(eventList.get(i).getString("name") + "\n\t" + eventList.get(i).getString("start_date"));
                 }
             }
-        });
-        if(eventList!=null) {
-            System.out.println(eventList.size());
-            for(int i = 0; i < eventList.size(); i++){
-                System.out.println(eventList.get(i).getString("name") + "\n\t" + eventList.get(i).getString("start_date"));
-            }
+        }
+        else {
+            System.out.println("NO INTERNET CONNECTION!!!");
         }
         //startActivity(new Intent(this, com.vandenrobotics.functionfirst.scout.ScoutActivity.class));
     }
@@ -95,5 +109,11 @@ public class MainActivity extends Activity {
                 return -1;
             }
         });
+    }
+
+    private boolean isOnline(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 }
