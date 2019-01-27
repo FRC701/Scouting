89f6acbdc6aa9c789edd2552d542e9403f442048
Kaylani Rose <rose.kaylani@gmail.com>
2019-01-27 10:49:53 -0800

    avgs layout started

---
+++ Stats/app/src/main/java/com/vandenrobotics/stats/activities/avgWeight.java
@@ -0,0 +1,15 @@
+package com.vandenrobotics.stats.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vandenrobotics.stats.R;
+
+public class avgWeight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avg_weight);
+    }
+}
